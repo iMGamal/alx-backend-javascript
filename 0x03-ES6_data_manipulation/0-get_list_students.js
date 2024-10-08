b2e5983b28e583eb9bb1e6d895c53f2eeb26e06e
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-08 22:27:44 +0000

    Test

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+let getListStudents {
+	const Guillaume = { id: 1, firstName: 'Guillaume', location: 'San Francesco' };
+	const James = { id: 2, firstName: 'James', location: 'Columbia' };
+	const Serena = { id: 3, firstName: 'Serena', location: 'San Francesco' };
+	let ArrayList = [Guillaume, James, Serena];
+	return ArrayList;
+}
