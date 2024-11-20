e97b3c082e5503f3c6603fd3140bcdb7b8c43c5d
iMGamal <mohamed.gamal126027@gmail.com>
2024-11-20 12:56:48 +0000

    Test

---
+++ 0x00-ES6_basic/7-getbudgetObject.js
@@ -0,0 +1,11 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+  
+  const {income, gdp, capita} = n_budget;
+
+  return n_budget;
+}
