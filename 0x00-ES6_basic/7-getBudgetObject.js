5e2de18cda3013013617a93383677d46e7458a2b
iMGamal <136174050+iMGamal@users.noreply.github.com>
2024-11-22 23:25:35 +0000

    Using shorthand syntax properly

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,4 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const income = this.income;
-  const gdp = this.gdp;
-  const capita = this.capita;
-
   const budget = {
     income,
     gdp,
