0d4458ad1075c1604e0dd2b07699a69b7d46b6c1
iMGamal <136174050+iMGamal@users.noreply.github.com>
2024-11-29 20:13:25 +0000

    Exit log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,7 +9,10 @@ if (process.stdin.isTTY) {
 else {
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
-    console.log('This important software is now closing');
     process.exit();
+  });
+
+  process.on('exit', () => {
+    console.log('This important software is now closing');
   });
 }
