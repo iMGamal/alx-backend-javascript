c88a7128d1a4cea508b2170c8c9e730727864946
iMGamal <136174050+iMGamal@users.noreply.github.com>
2024-11-29 20:07:40 +0000

    TTY, stdin and stdout

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,14 @@ console.log('Welcome to Holberton School, what is your name?');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 }
 else {
   process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    console.log('This important software is now closing');
     process.exit();
   });
-  console.log('This important software is now closing');
 }
