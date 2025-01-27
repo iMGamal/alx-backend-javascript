5c704181c49909ce68a5eafc6c650e9e8cf2d2e4
iMGamal <mohamed.gamal126027@gmail.com>
2025-01-27 12:41:44 +0000

    Just as before but Holberton School instead

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,11 @@ const readline = require('readline');
 
 const r1 = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
-  terminal: process.stdin.isTTY
+  terminal: process.stdin.isTTY,
+  output: process.stdout
 });
 
-r1.question('Welcome to ALX, what is your name?\n', (name) => {
+r1.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}\r`);
   if (!process.stdin.isTTY) {
     console.log("This important software is now closing\r");
